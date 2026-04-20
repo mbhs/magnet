46beca7a1992461325f781ec6063109dd4a39b7a
danyulgh <daniel.g.hoang2007@gmail.com>
2026-04-20 19:13:48 -0400

    temp

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,12 @@
 // app/providers.tsx
 'use client'
-import {HeroUIProvider} from '@heroui/react'
+// import { HeroUIProvider } from '@heroui/react'
 
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider>
+    // <HeroUIProvider>
       {children}
-    </HeroUIProvider>
+    // </HeroUIProvider>
   )
 }
